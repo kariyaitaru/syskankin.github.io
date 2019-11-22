f2b5ad1354b1c2f32a793960a1188e5b8adac5e1
Kinjo <e.kinjo@syscon.gr.jp>
2019-11-22 15:11:05 +0900

    test

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ window.onload = () => {
 
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-               .register('./sw.js');
+        .register('./sw.js')
+        .then(registration => {
+          // 登録成功
+          registration.onupdatefound = function() {
+            console.log('アップデートがあります！');
+            registration.update();
+          }
+        })
+        .catch(err => {
+          // 登録失敗
+      });
     }
   }
--- sw.js
+++ sw.js
@@ -23,18 +23,3 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/serviceworker.js')
-    .then(registration => {
-      // 登録成功
-      registration.onupdatefound = function() {
-        console.log('アップデートがあります！');
-        registration.update();
-      }
-    })
-    .catch(err => {
-      // 登録失敗
-  });
-}
